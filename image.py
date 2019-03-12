c8a1a31bdb8be9b90b731627aee16615bce0ad49
Evan Geary <geary@berkeley.edu>
2019-03-12 14:55:22 -0700

    added append and reshape to image.py

--- image.py
+++ image.py
@@ -2,8 +2,18 @@ import imageio
 import numpy as np
 
 class Image:
-  def __init__(self, filename):
-    self._baseImage = imageio.imread(filename)
+  def __init__(self, filename=None, base=None):
+    self._baseImage = None
+    if filename != None:
+      self._baseImage = imageio.imread(filename)
+      self.addAlphas()
+    elif base.any() != None:
+      self._baseImage = base
+      self.addAlphas()
+ 
+
+  '''adds alpha values if they aren't there.'''
+  def addAlphas(self):
     if(self._baseImage.shape[2] == 3):
       alphas = np.array([[[np.uint8(255)] for i in range(self._baseImage.shape[0])] for j in range(self._baseImage.shape[1])])
       self._baseImage = np.append(self._baseImage, alphas, axis=2)
@@ -13,6 +23,12 @@ class Image:
 
   def scalarToY(self, scale):
     return int(self._baseImage.shape[0] * scale)
+
+  def size(self):
+    return self._baseImage.shape
+
+  def base(self):
+    return self._baseImage[:]
 
   '''other: other image. x: x position of upper left of other. y: same but y coord.'''
   def overlay(self, other, x, y):
@@ -39,12 +55,25 @@ class Image:
       return self._baseImage[pos:]
     if(nesw.upper() == 'W'):
       return self._baseImage[:,:pos]
-    return self._baseImage
-
+    return self.base()
   
+  '''other: other picture. nesw: direction other picture is of self.'''
+  def append(self, other, nesw):
+    if(nesw.upper() == 'N' and self.size()[1] == other.size()[1]):
+      return np.append(other._baseImage, self._baseImage, axis=0)
+    if(nesw.upper() == 'E' and self.size()[0] == other.size()[0]):
+      return np.append(self._baseImage, other._baseImage, axis=1)
+    if(nesw.upper() == 'S' and self.size()[1] == other.size()[1]):
+      return np.append(self._baseImage, other._baseImage, axis=0)
+    if(nesw.upper() == 'W' and self.size()[0] == other.size()[0]):
+      return np.append(other._baseImage, self._baseImage, axis=1)
+    return self.base()
 
-
-
+  '''x: x size of the returned image. y: same but for y'''
+  def reshape(self, x, y):
+    x1 = self.size()[1]
+    y1 = self.size()[0]
+    return np.array([[self._baseImage[int(j*y1/y),int(i*x1/x)] for i in range(x)] for j in range(y)])
 
 
 
--- test.py
+++ test.py
@@ -2,7 +2,9 @@ import imageio
 import numpy as np
 import image
 
-cat = image.Image('Cat.png')
-bloon = image.Image('Balloon.png')
-world = image.Image('world.png')
-imageio.imwrite('test.png', world.overlay(bloon, 150, 150))
+cat = image.Image('pngs/Cat.png')
+bloon = image.Image('pngs/Balloon.png')
+world = image.Image('pngs/world.png')
+world2 = image.Image('pngs/world2.png')
+twoalt = image.Image(base=world2.divide(world2.scalarToX(0.5), 'e'))
+imageio.imwrite('test.png', world.reshape(200, 200))
